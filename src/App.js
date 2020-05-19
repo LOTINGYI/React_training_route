2e154ae427622fc115aef4152c80b2c7a5dd6df9
timlo <a0987642936@yahoo.com>
2020-05-19 22:00:01 +0800

    Output list in array and refactor repeated and trivial code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Person from './Person/Person'
+export default class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Martin', age: 25 },
+      { name: 'Alex', age: 27 }
+    ],
+    showPersons: false
+  }
+
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Martin', age: 25 },
+        { name: 'Alex', age: 21 }
+      ]
+    })
+  }
+
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 25 },
+        { name: 'Alex', age: 21 }
+      ]
+    })
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({
+      showPersons: !doesShow
+    })
+  }
+  render() {
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map(person => (
+            <Person
+              name={person.name}
+              age={person.age} />
+          ))}
+        </div>
+      )
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi shut the fuck up</h1>
+        <button onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {this.state.showPersons ? persons : null}
+
+      </div>
+    )
+  }
 }
-
-export default App;
