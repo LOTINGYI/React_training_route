ce02f8ad3fe66b1ab745b07d058fadc6582ea633
timlo <a0987642936@yahoo.com>
2020-05-23 22:15:27 +0800

    Using Pseudo Selectors

--- src/App.js
+++ src/App.js
@@ -60,19 +60,9 @@ class App extends Component {
     })
   }
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
-    }
+    
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
@@ -88,11 +78,7 @@ class App extends Component {
         </div>
       )
 
-      style.backgroundColor = 'red'
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      }
+      btnClass = classes.Red
     }
 
     const assignedClasses = []
@@ -109,7 +95,7 @@ class App extends Component {
         <h1>Hi shut the fuck up</h1>
         <p className={assignedClasses.join(' ')}>This is really working</p>
         <button
-          style={style}
+          className={btnClass}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {this.state.showPersons ? persons : null}
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,9 @@
 import React from "react";
-import './Person.css'
+import classes from './Person.css'
 const person = (props) => {
  
     return (
-        <div className="Person" >
+        <div className={classes.Person} >
             <p onClick={props.click}>Get the fuck up {props.name} your age is {props.age}!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
